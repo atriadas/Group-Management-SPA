77a98d7149db10cc052015c425f737e164a3abd0
Atria Das <atria.das@mitel.com>
2020-06-11 15:06:30 +0530

    11-06 2nd

--- src/app/manage/manage.component.spec.ts
+++ src/app/manage/manage.component.spec.ts
@@ -11,89 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { defer, Observable, of } from 'rxjs';
-//import { AddFeatureUserComponent } from './add-feature-user.component';
+
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Data2Service } from '../dataService/data2.service';
 import { ManageComponent } from './manage.component';
 import { NgSelectComponent, NgSelectModule } from '@ng-select/ng-select';
-//import { DataServiceStub } from '../../services.stub/data.service.stub';
 
-//
-// Helper functions for HttpClient spys that return rxjs Observables.
-//
-// from https://angular.io/guide/testing#async-observable-helpers
-/** Create async observable that emits-once and completes
-*  after a JS engine turn */
-// export function asyncData<T>(data: T) {
-//     return defer(() => Promise.resolve(data));
-// }
-// /** Create async observable error that errors
-// *  after a JS engine turn */
-// export function asyncError<T>(errorObject: any) {
-//     return defer(() => Promise.reject(errorObject));
-// }
-
-// describe('AddFeatureUserComponent', () => {
-//     let component: AddFeatureUserComponent;
-//     let fixture: ComponentFixture<AddFeatureUserComponent>;
-//     let spy_logged_in_user;
-
-//     class ActivatedRouteMock {
-//         queryParams = new Observable(observer => {
-//             const urlParams = {
-//                 useruuid: 1,
-//                 tenantuuid: 2
-//             };
-//             observer.next(urlParams);
-//             observer.complete();
-//         });
-//     }
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             declarations: [AddFeatureUserComponent],
-//             imports: [FormsModule, NgbModule, HttpClientModule, RouterTestingModule],
-//             providers: [
-//                 {
-//                     provide: ActivatedRoute,
-//                     useValue: ActivatedRouteMock
-//                 },
-//             ],
-//             schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
-//         })
-//             .compileComponents();
-//     });
-
-//     beforeEach(() => {
-//         spy_logged_in_user = spyOn(TestBed.get(DataServiceStub), 'getLoggedInUser').and.returnValue(asyncData(true));
-//         fixture = TestBed.createComponent(AddFeatureUserComponent);
-//         component = fixture.componentInstance;
-//         TestBed.get(ActivatedRoute).queryParams = of({ useruuid: 1, tenantuuid: 2 });
-//         fixture.detectChanges();
-//     });
-
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-
-//     it('error on getting loggedin user', () => {
-//         spy_logged_in_user.and.returnValue(asyncError('an error occurred'));
-//         expect(component.errorflag).toBeTruthy();
-//     });
-
-    //   it('should fetch value for userId and tid on ngoninit', async(() => {
-    //     component.ngOnInit();
-    //     let route= TestBed.get(ActivatedRoute)
-    //     route.subscribe(x=>{
-    //         route=x})
-    //      component.userId=route
-    //     //component.tid=1
-    //     expect(component.userId).toEqual('x_115');
-    //    // expect(component.tid).toBeDefined();
-    //     // expect(component.userId).toEqual('');
-
-
-    //   }));
-//});
 
 
 
@@ -103,6 +26,15 @@ describe('ManageComponent:getUserInfo', () => {
     let mockService;
     let component: ManageComponent;
     let fixture: ComponentFixture<ManageComponent>;
+    const ServiceStub = {
+        getUserData() {
+          return of( [{UserRole: "All Recordings",
+          MembersOfGroup: [{group_uuid:"aa781f9f-8e34-45b8-98e3-e0cb69bfd4bc",
+                            group_name:"Mitel Group 1"}],
+          ManagersOfGroup:[{group_uuid:"aa781f9f-8e34-45b8-98e3-e0cb69bfd4bc",
+                             group_name:"Mitel Group 1"}]}] )
+        }
+      }
 
 
     // class MockService {
@@ -122,12 +54,16 @@ describe('ManageComponent:getUserInfo', () => {
 
 
     beforeEach(async(() => {
-        mockService = jasmine.createSpyObj(['getUserData']);
-        mockService.getUserData.and.returnValue("None");
+        // mockService = jasmine.createSpyObj(['getUserData']);
+        // mockService.getUserData.and.returnValue( of({UserRole: "All Recordings",
+        //                      MembersOfGroup: [{group_uuid:"aa781f9f-8e34-45b8-98e3-e0cb69bfd4bc",
+        //                                        group_name:"Mitel Group 1"}],
+        //                      ManagersOfGroup:[{group_uuid:"aa781f9f-8e34-45b8-98e3-e0cb69bfd4bc",
+        //                                         group_name:"Mitel Group 1"}]}));
              TestBed.configureTestingModule({
               declarations: [ ManageComponent],
               imports: [NgSelectModule, FormsModule, NgbModule, HttpClientModule, RouterTestingModule],
-              providers: [ { provide: Data2Service, useValue: mockService } ]
+              providers: [ { provide: Data2Service, useValue: ServiceStub } ]
              })
              
              .compileComponents();
@@ -155,8 +91,8 @@ describe('ManageComponent:getUserInfo', () => {
     });
 
     it('should getUserInfo', async(() => {
-        component.getuserinfo()
-        expect(component.role_id).toEqual(1);
+        fixture.detectChanges();
+        expect(component.role_id).toEqual(4);
     }));
 })
    
